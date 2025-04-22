375714b44af9b5e4df219c53fde6d73fb67c163c
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-22 15:32:09 -0400

    feat: replace div elements with Link components for navigation to the home page

--- exercises/02.routing/02.problem.links/app/root.tsx
+++ exercises/02.routing/02.problem.links/app/root.tsx
@@ -2,6 +2,7 @@ import os from 'node:os'
 import { cssBundleHref } from '@remix-run/css-bundle'
 import { json, type LinksFunction } from '@remix-run/node'
 import {
+	Link,
 	Links,
 	LiveReload,
 	Outlet,
@@ -36,11 +37,10 @@ export default function App() {
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
 				<header className="container mx-auto py-6">
 					<nav className="flex justify-between">
-						{/* 🐨 switch this div to a Link from @remix-run/react and link to "/" */}
-						<div>
+						<Link to="/">
 							<div className="font-light">epic</div>
 							<div className="font-bold">notes</div>
-						</div>
+						</Link>
 					</nav>
 				</header>
 
@@ -49,11 +49,10 @@ export default function App() {
 				</div>
 
 				<div className="container mx-auto flex justify-between">
-					{/* 🐨 switch this div to a Link from @remix-run/react and link to "/" */}
-					<div>
+					<Link to="/">
 						<div className="font-light">epic</div>
 						<div className="font-bold">notes</div>
-					</div>
+					</Link>
 					<p>Built with ♥️ by {data.username}</p>
 				</div>
 				<div className="h-5" />
