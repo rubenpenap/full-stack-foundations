37230477d235406dd5d6b95d368fb8a17040e764
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-22 12:08:39 -0400

    feat: add cssBundleHref to links array in root component

--- exercises/01.styling/05.problem.bundling-css/app/root.tsx
+++ exercises/01.styling/05.problem.bundling-css/app/root.tsx
@@ -1,11 +1,9 @@
+import { cssBundleHref } from '@remix-run/css-bundle'
 import { type LinksFunction } from '@remix-run/node'
 import { Links, LiveReload, Scripts } from '@remix-run/react'
 import faviconAssetUrl from './assets/favicon.svg'
 import { EpicShop } from './epicshop.tsx'
-// 🐨 Import the cssBundleHref here
 import fontStylesheetUrl from './styles/font.css'
-// 🧝‍♂️ I imported the CSS file for you. When you have it working, it'll be obvious
-// ... I promise... 😈
 import './styles/global.css'
 import tailwindStylesheetUrl from './styles/tailwind.css'
 
@@ -14,10 +12,8 @@ export const links: LinksFunction = () => {
 		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
 		{ rel: 'stylesheet', href: fontStylesheetUrl },
 		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
-		// 🐨 Add the cssBundleHref to the links array
-		// 🦺 feel free to handle the undefined case however you like or ignore
-		// the TypeScript error if you want. I'll show you how I handle it later.
-	]
+		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
+	].filter(Boolean)
 }
 
 export default function App() {
