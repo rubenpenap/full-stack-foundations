032ae8365c4bc9afe861c7f7cc16e97c45c633a1
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-28 20:08:12 -0400

    feat: implement action function to delete notes and redirect to user's notes page

--- exercises/04.mutations/04.problem.button-forms/app/routes/users+/$username_+/notes.$noteId.tsx
+++ exercises/04.mutations/04.problem.button-forms/app/routes/users+/$username_+/notes.$noteId.tsx
@@ -1,5 +1,10 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import {
+	json,
+	redirect,
+	type ActionFunctionArgs,
+	type LoaderFunctionArgs,
+} from '@remix-run/node'
+import { Form, Link, useLoaderData } from '@remix-run/react'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { db } from '#app/utils/db.server.ts'
@@ -21,9 +26,10 @@ export async function loader({ params }: LoaderFunctionArgs) {
 	})
 }
 
-// 🐨 export an action function that uses the params from the ActionFunctionArgs
-//  🐨 delete the note from the database (💰 db.note.delete({ where: { id: { equals: params.noteId } } }))
-//  🐨 return a redirect to the user's notes page
+export async function action({ params }: ActionFunctionArgs) {
+	db.note.delete({ where: { id: { equals: params.noteId } } })
+	return redirect(`/users/${params.username}/notes`)
+}
 
 export default function NoteRoute() {
 	const data = useLoaderData<typeof loader>()
@@ -37,13 +43,11 @@ export default function NoteRoute() {
 				</p>
 			</div>
 			<div className={floatingToolbarClassName}>
-				{/* 🐨 wrap this Button in a Form with the proper method */}
-				<Button
-					// 🐨 add a type="submit" prop to this Button
-					variant="destructive"
-				>
-					Delete
-				</Button>
+				<Form method="POST">
+					<Button type="submit" variant="destructive">
+						Delete
+					</Button>
+				</Form>
 				<Button asChild>
 					<Link to="edit">Edit</Link>
 				</Button>
