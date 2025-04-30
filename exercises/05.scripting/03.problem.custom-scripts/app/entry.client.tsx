4cad16c030dc4e57cbb01a8565c42fe017809190
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-29 20:38:51 -0400

    feat: add conditional import for development environment to initialize devtools

--- exercises/05.scripting/03.problem.custom-scripts/app/entry.client.tsx
+++ exercises/05.scripting/03.problem.custom-scripts/app/entry.client.tsx
@@ -2,8 +2,10 @@ import { RemixBrowser } from '@remix-run/react'
 import { startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
-// 🐨 add an if statement here that checks if the ENV.MODE is development
-// 🐨 if it is, then: import('./utils/devtools.tsx').then(({ init }) => init())
+if (ENV.MODE === 'development') {
+	await import('./utils/devtools.tsx').then(({ init }) => init())
+}
+
 startTransition(() => {
 	hydrateRoot(document, <RemixBrowser />)
 })
